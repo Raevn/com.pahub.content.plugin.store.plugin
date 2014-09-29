41085211ec91c03862cebdcf9045961fa6adb5a5
Raevn <d_bened@raevn.com>
2014-09-29 22:24:19 +1000

    2014-09-29-01
    Change: removed superfluous log messages

--- com.pahub.content.plugin.store.plugin/com.pahub.content.plugin.store.plugin.js
+++ com.pahub.content.plugin.store.plugin/com.pahub.content.plugin.store.plugin.js
@@ -6,15 +6,12 @@ function unload_store_plugin(data) {}
 
 function store_plugin_enabled(content) {
 	if (pahub.api.plugin.getPluginLoaded(content.content_id) == false) {
-		pahub.api.plugin.loadPlugin(content.local_path, function(content) {
-			pahub.api.log.addLogMessage("info", "Plugin '" + content.content_id + "': enabled");
-		});
+		pahub.api.plugin.loadPlugin(content.local_path);
 	}
 }
 
 function store_plugin_disabled(content) {
 	if (pahub.api.plugin.getPluginLoaded(content.content_id) == true) {
 		pahub.api.plugin.unloadPlugin(content.content_id);
-		pahub.api.log.addLogMessage("info", "Plugin '" + content.content_id + "': disabled");
 	}
 }
