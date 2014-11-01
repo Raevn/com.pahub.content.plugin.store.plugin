5e611219cbe405ea646fbc4cf64d9fc29561dbad
Raevn <d_bened@raevn.com>
2014-11-01 16:56:36 +1100

    2014-11-01-01
    Change: Now creates a meta-content item for pahub, so content can depend
    on specific versions.

--- com.pahub.content.plugin.store.plugin/com.pahub.content.plugin.store.plugin.js
+++ com.pahub.content.plugin.store.plugin/com.pahub.content.plugin.store.plugin.js
@@ -1,5 +1,20 @@
 function load_store_plugin(data) {
 	pahub.api.content.addContentStore(data.content_id, data.display_name, data);
+	var pahubPlugin = {
+		"content_id": "com.pahub",
+		"store_id": "com.pahub.content.plugin.store.plugin",
+		"display_name": "PA Hub",
+		"description": "Plugin representing PA Hub, for dependency/versioning purposes",
+		"author": "Raevn",
+		"version": pahubPackage.version,
+		"date": "2014/10/25",
+		"enabled": true
+	};
+	model.loaded_plugins.push(pahubPlugin);
+	model.core_plugins.push(pahubPlugin);
+	pahub.api.content.addContentItem(true, pahubPlugin.store_id, pahubPlugin.content_id, pahubPlugin.display_name, "", pahubPlugin);
+	
+	//TODO: Add PA plugin.
 }
 
 function unload_store_plugin(data) {}
