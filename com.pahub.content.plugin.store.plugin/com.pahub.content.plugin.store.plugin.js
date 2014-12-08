9b560def8f4f8b58a7f652e4b8c04ac6256d9a48
Raevn <d_bened@raevn.com>
2014-12-09 06:33:57 +1100

    Update to v0.3.0
    Change: New icon for Planetary Annnihilation meta content
    Change: New icon for PA Hub meta content
    Change: Now uses stream system
    Change: Planetary Annihilation Meta content now keeps a fixed name

--- com.pahub.content.plugin.store.plugin/com.pahub.content.plugin.store.plugin.js
+++ com.pahub.content.plugin.store.plugin/com.pahub.content.plugin.store.plugin.js
@@ -1,4 +1,14 @@
-function load_store_plugin(data) {
+/*
+	Plugin "Plugin Store"
+	=====================
+	(Store) Manage installed Plugins
+	
+	content_id: com.pahub.content.plugin.store.plugin
+	store_id: com.pahub.content.plugin.store.plugin
+	author: Raevn
+*/
+
+function load_store_plugin(data, folder) {
 	pahub.api.content.addContentStore(data.content_id, data.display_name, data);
 	var pahubPlugin = {
 		"content_id": "com.pahub",
@@ -7,27 +17,24 @@ function load_store_plugin(data) {
 		"description": "Plugin representing PA Hub, for dependency/versioning purposes",
 		"author": "Raevn",
 		"version": pahubPackage.version,
-		"enabled": true
+		"enabled": true,
+		"icon": path.join(folder, "pahub.png")
 	};
 	model.loaded_plugins.push(pahubPlugin);
 	model.core_plugins.push(pahubPlugin);
 	pahub.api.content.addContentItem(true, pahubPlugin.store_id, pahubPlugin.content_id, pahubPlugin.display_name, "", pahubPlugin);
 	
-	//Needed?
-	if (model.pte_build() || model.stable_build()) {
+	if (Object.keys(streams).length > 0 && model.stream() != "") {
 		var paPlugin = {
 			"content_id": "com.pa",
 			"store_id": "com.pahub.content.plugin.store.plugin",
-			"display_name": "PA PTE Stream",
-			"description": "Plugin representing PA (PTE stream), for dependency/versioning purposes",
+			"display_name": "Planetary Annihilation",
+			"description": "Plugin representing Planetary Annihilation, for dependency/versioning purposes",
 			"author": "Uber Entertainment",
-			"version": parseInt(model.pte_build()) + ".0.0",
-			"enabled": true
+			"version": pahub.api.content.fixVersionString(streams[model.stream()].build + ".0.0"),
+			"enabled": true,
+			"icon": path.join(folder, "pa.png")
 		};
-		
-		if (model.pte_build().indexOf("-") > -1) {
-			paPlugin.version += model.pte_build().substring(model.pte_build().indexOf("-"), model.pte_build().length);
-		}
 		model.loaded_plugins.push(paPlugin);
 		model.core_plugins.push(paPlugin);
 		pahub.api.content.addContentItem(true, paPlugin.store_id, paPlugin.content_id, paPlugin.display_name, "", paPlugin);
@@ -38,42 +45,16 @@ function load_store_plugin(data) {
 
 var setPAplugin = function() {
 	var content = pahub.api.content.getContentItem(true, "com.pa");
-	if (model.stream() == "STEAM") {
-		content.data.display_name = "PA (Steam)";
-		content.display_name("PA (Steam)");
-		content.data.description = "Plugin representing PA (Steam), for dependency/versioning purposes";
-		content.data.version = parseInt(model.stable_build()) + ".0.0";
-		if (model.stable_build().indexOf("-") > -1) {
-			content.data.version += model.stable_build().substring(model.stable_build().indexOf("-"), model.stable_build().length);
-		}
-		content.version(pahub.api.content.fixVersionString(content.data.version));		
+	if (pahub.api.content.contentItemExists(true, "com.pa") == true) {
+		content.version(pahub.api.content.fixVersionString(streams[model.stream()].build + ".0.0"));	
+		model.content.getContentItems(true).valueHasMutated();
+		pahub.api.content.verifyContentDependencies();
 	}
-	if (model.stream() == "STABLE") {
-		content.data.display_name = "PA Stable Stream";
-		content.display_name("PA Stable Stream");
-		content.data.description = "Plugin representing PA (Stable stream), for dependency/versioning purposes";
-		content.data.version = parseInt(model.stable_build()) + ".0.0";
-		if (model.stable_build().indexOf("-") > -1) {
-			content.data.version += model.stable_build().substring(model.stable_build().indexOf("-"), model.stable_build().length);
-		}
-		content.version(pahub.api.content.fixVersionString(content.data.version));			
-	}
-	if (model.stream() == "PTE") {
-		content.data.display_name = "PA PTE Stream";
-		content.display_name("PA PTE Stream");
-		content.data.description = "Plugin representing PA (PTE stream), for dependency/versioning purposes";
-		content.data.version = parseInt(model.pte_build()) + ".0.0";
-		if (model.pte_build().indexOf("-") > -1) {
-			content.data.version += model.pte_build().substring(model.pte_build().indexOf("-"), model.pte_build().length);
-		}
-		content.version(pahub.api.content.fixVersionString(content.data.version));			
-	}
-	
-	model.content.getContentItems(true).valueHasMutated();
-	pahub.api.content.verifyContentDependencies();
 }
 
-function unload_store_plugin(data) {}
+function unload_store_plugin(data) {
+	//This plugin is core; it cannot be unloaded
+}
 
 function store_plugin_enabled(content) {
 	if (pahub.api.plugin.getPluginLoaded(content.content_id) == false) {
